6b19e448558da49fa24011cb7b21f775b7f35584
nicogid <nicolas.gidon@gmail.com>
2017-06-07 19:16:50 +0200

    Au

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,15 @@
+from django.utils import timezone
+import datetime
+
 from django.db import models
 
-# Create your models here.
+def get_expiration_date():
+    return timezone.now() + datetime.timedelta(day=1)
+
+class Token(models.Model):
+    user = models.OneToOneField('auth.User',on_delete=models.CASCADE,unique=True)
+    hash = models.CharField(max_length=100)
+    expiration_date = models.DateTimeField(default=get_expiration_date)
+
+    def is_expired(self):
+        return self.expiration_date < timezone.now()
